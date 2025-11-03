5b16bb0fc15f80d581060f2677b0cc171531689d
hellowurld-create <akpagodswill2@outlook.com>
2025-11-03 20:05:48 +0100

    update: improved about page

--- components/LandingPage/About.tsx
+++ components/LandingPage/About.tsx
@@ -30,23 +30,6 @@ const About: React.FC = () => {
 		if (inView) controls.start('visible');
 	}, [controls, inView]);
 
-	const containerVariants = {
-		hidden: { opacity: 0 },
-		visible: {
-			opacity: 1,
-			transition: { staggerChildren: 0.2, delayChildren: 0.3 },
-		},
-	};
-
-	const itemVariants = {
-		hidden: { y: 20, opacity: 0 },
-		visible: {
-			y: 0,
-			opacity: 1,
-			transition: { duration: 0.5, ease: 'easeOut' },
-		},
-	};
-
 	const handleEmailSubmit = (email: string) => {
 		console.log('User email (About):', email);
 	};
@@ -64,24 +47,21 @@ const About: React.FC = () => {
 						ref={ref}
 						initial='hidden'
 						animate={controls}
-						variants={containerVariants}
 						className='absolute inset-0 flex flex-col items-center justify-end mb-10 text-center text-black z-10 makeAbout'>
 						<h6
 							// variants={itemVariants}
 							className='text-[45px] text-[#292929] font-medium mb-6'>
 							About Neurtask
 						</h6>
-						<motion.p
-							variants={itemVariants}
+						<p
 							 className='text-lg lg:text-xl mb-10 text-[#292929] leading-relaxed font-medium'>
 							{/* className='text-[28px] font-medium text-[#292929] leading-[50px] text-center mb-10'>	 */}
 							Experience a new level of simplicity with AI that enhances the way
 							<br className='lg:flex hidden' /> you work, create, and live.
-						</motion.p>
+						</p>
 
 						{/* Inline email popup (always visible) */}
 						<motion.div
-							variants={itemVariants}
 							className='flex justify-center w-full '>
 							<EmailPopup
 								isVisible={true}
